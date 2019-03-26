0ac0101ec7113e056cf23d91e1fee9c6defe315f
Leo Xiong <hello@leoxiong.com>
2019-03-26 19:08:00 +1300

    Truncate DuckDuckGo search result description

--- conroy/plugin/duckduckgo.py
+++ conroy/plugin/duckduckgo.py
@@ -2,6 +2,7 @@ import logging
 
 from selenium.common.exceptions import NoSuchElementException, InvalidSelectorException
 
+from conroy.utils import truncate_ellipses
 from ..decorator import hook, parameter
 from ..plugin.conroyplugin import ConroyPlugin
 
@@ -17,7 +18,7 @@ class DuckDuckGo(ConroyPlugin):
             result = self.resource['Selenium.driver'].find_element_by_css_selector('#r1-0')
             title = result.find_element_by_css_selector('div > h2 > a.result__a').text
             url = result.find_element_by_css_selector('div > h2 > a.result__a').get_attribute('href')
-            description = result.find_element_by_css_selector('div > div.result__snippet.js-result-snippet').text
+            description = truncate_ellipses(result.find_element_by_css_selector('div > div.result__snippet.js-result-snippet').text, 384)
 
             return ['{} - {}'.format(title, url), description]
         except (NoSuchElementException, InvalidSelectorException) as e:
